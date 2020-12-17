4ddb8b58743a9f305873874aa9c31f09bdaff7bf
Deen17 <adilriazudeen@gmail.com>
2020-12-17 13:45:36 -0600

    Create config.rs

---
+++ src/network/config.rs
@@ -0,0 +1,7 @@
+pub static AUTHTOKEN: &str = "your DGG login key";
+
+pub static NICK: &str = "BOT NAME"; // the dgg name of your bot
+
+pub static DEV: &str = "DEV NAME"; // the developer's name for testing
+
+pub static INITIAL_POINTS: usize = 500; // could be anything
