72268953aec8186a50983bc9adef800fa1bb4ff8
Adil Desktop <adilriazudeen@gmail.com>
2020-12-17 21:46:52 -0600

    pie

--- dggbet/src/app/app.module.ts
+++ dggbet/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dggbet/src/app/pie/pie.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {ChartType, ChartOptions} from'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
+@Component({
+  selector: 'bet-pie',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.css']
+})
+export class PieComponent implements OnInit {
+   // Pie
+   public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
